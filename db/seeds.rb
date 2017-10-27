7c08ae55107b18dda5fda236fb978a1a0bbb2447
Paweł Zwoliński <p.zwolin@gmail.com>
2017-10-27 18:45:01 +0200

    Add deadline to Task

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,18 +10,22 @@ Task.destroy_all
 
 Task.create!([{
   title: "Pimp this app",
-  description: "Pimp this TODO app and make it work as perfect as possible."
+  description: "Pimp this TODO app and make it work as perfect as possible.",
+  deadline: DateTime.now + 5.days
 },
 {
   title: "Sign in for workshops",
-  description: "Fill in the workshops registration form and click 'submit'!"
+  description: "Fill in the workshops registration form and click 'submit'!",
+  deadline: DateTime.now + 10.days
 },
 {
   title: "Empty your stomach",
-  description: "Prepare your stomach for pizza during workshops"
+  description: "Prepare your stomach for pizza during workshops",
+  deadline: DateTime.now + 15.days
 },
 {
-  title: "Tell the world about the workshops!"
+  title: "Tell the world about the workshops!",
+  deadline: DateTime.now + 20.days
 }
 ])
 
